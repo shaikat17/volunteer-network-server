0f814d1518fff44027cd199e3f902225271b741d
shaikat17 <shaikatpal56@gmail.com>
2023-05-16 12:47:53 +0600

    add event route added

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+// Replace the uri string with your connection string.
+const uri = "mongodb://127.0.0.1:27017/";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const vonlunteerNetworkDB = client.db('vonlunteerNetworkDB');
+    const VNDBEvents = vonlunteerNetworkDB.collection('VNDBEvents');
+
+    app.post('/add-event', async (req, res) => {
+        const data = req.body
+        
+        const result = await VNDBEvents.insertOne(data)
+        res.send(result)
+    })
+
+    console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req,res) => {
     res.send('Volunteer Network')
